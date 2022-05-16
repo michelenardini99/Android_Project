5fbdcdb844dd1ad9f6a2a5e9546a65b0d46d57cb
Michele Nardini <michele.nardini3g@gmail.com>
2022-05-16 19:32:52 +0200

    create login, navigation bar and navigation drawer

--- app/src/main/java/com/example/dairys/MainActivity.java
+++ app/src/main/java/com/example/dairys/MainActivity.java
@@ -1,14 +1,115 @@
 package com.example.dairys;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dairys.Fragment.DreamDiaryFragment;
+import com.example.dairys.Fragment.HomeFragment;
+import com.example.dairys.Fragment.SettingsFragment;
+import com.example.dairys.Fragment.StatisticsFragment;
+import com.example.dairys.databinding.ActivityMainBinding;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity  {
+
+    ActivityMainBinding binding;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private MaterialToolbar toolbar;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_drawer);
+
+        binding.bottomNavigation.setOnItemSelectedListener( item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.statistics:
+                    replaceFragment(new StatisticsFragment());
+                    break;
+                case R.id.dream_diary:
+                    replaceFragment(new DreamDiaryFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+            return true;
+        });
+
+        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_closed);
+
+        drawerLayout.setDrawerListener(toggle);*/
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /*@Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.profile:
+                Toast.makeText(getApplicationContext(), "Click profile button", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.dream_board:
+                Toast.makeText(getApplicationContext(), "Click dream board button", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.favorites:
+                Toast.makeText(getApplicationContext(), "Click favorites button", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawers();
+                break;
+        }
+
+        return true;
+    }*/
 }
